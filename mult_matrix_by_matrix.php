0e7bd658aa4d72b8ddb3215de12f468ca6da251d
Zolotukhin Fedor <bat312@yandex.ru>
2019-03-01 11:57:06 +0300

    Make html output in matrix_by_matrix

--- mult_matrix_by_matrix.php
+++ mult_matrix_by_matrix.php
@@ -15,14 +15,47 @@
         return $result;
     }
 
-    $a[0][0] = 1; $a[0][1] = 2; $a[1][0] = 3; $a[1][1] = 4; 
-    $b[0][0] = 1; $b[0][1] = 0; $b[1][0] = 0; $b[1][1] = 1;
-    $res = multiple_matrix_by_matrix($a, $b);
+    function render_table($values, $class_name) {
+        $result = '<table class= "'.$class_name.'">';
 
-    for($i = 0; $i < count($res); $i++) {
-        for ($j = 0; $j < count($res); $j++) {
-            echo $res[$i][$j]." ";
+        for($i = 0; $i < count($values); $i++) {
+            $result.="<tr>";
+
+            for($j = 0; $j < count($values[0]); $j++) {
+                $result.="<td>".(string)$values[$i][$j]."</td>";
+            }
+
+            $result.="</tr>";
         }
-        echo "\n";
+
+        $result.="</table>";
+
+        return $result;
     }
+
+    function render() {
+        $a[0][0] = 1; $a[0][1] = 2; $a[1][0] = 3; $a[1][1] = 4; 
+        $b[0][0] = 1; $b[0][1] = 0; $b[1][0] = 0; $b[1][1] = 1;
+        
+        $result =
+        '<html>
+            <head>
+                <title>Multiple Matrix By Matrix</title>
+                <link rel="stylesheet"  href="style.css"/>
+            </head>
+            <body>';
+
+            $result.=render_table($a, 'matrix');
+            $result.=' * ';
+            $result.=render_table($b, 'matrix');
+            $result.=' = ';
+            $result.=render_table(multiple_matrix_by_matrix($a, $b), 'matrix');
+
+            $result.='</body></html>';
+
+            return $result;
+    }
+
+    echo render();
+
 ?>
